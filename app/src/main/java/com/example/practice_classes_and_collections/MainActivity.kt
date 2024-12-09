88a915b5ffc4b1ac052b19c9b7d4082afaf6f071
A-Shermatov <azamatjon.shermatov@gmail.com>
2024-12-09 17:49:25 +0300

    Task 1

--- app/src/main/java/com/example/practice_classes_and_collections/MainActivity.kt
+++ app/src/main/java/com/example/practice_classes_and_collections/MainActivity.kt
@@ -1,6 +1,14 @@
 package com.example.practice_classes_and_collections
 
 
+// Task 1
+data class Event(
+    val title: String,
+    val description: String? = null,
+    val dayPart: String,
+    val durationInMinutes: Int,
+)
+
 fun main() {
     println("Hello world!")
 }
